e2eeec7add0f2c3c84684e011b753b7b03ac0bf0
Carlos Rivero Molina <Carlos Rivero Molina>
2016-04-25 22:43:11 +0200

    Changed port to work with heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/app'));
 
-app.listen(8080, function () {
-	console.log('App listening on port 8080');
+app.listen(port, function () {
+	console.log('App listening on port' + port);
 });
