ac8e4f6c2607f887a9f81e26142d781acaa68359
Carlos Rivero Molina <Carlos Rivero Molina>
2016-04-26 13:55:56 +0200

    Added google authentication

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,10 +7,30 @@ var bookusControllers = angular.module('bookusControllers', ["firebase"]);
 bookusControllers.controller('BookListCtrl', ['$scope', '$firebaseArray',
   function($scope, $firebaseArray) {
 	var ref = new Firebase("https://bookus.firebaseio.com/");
-	// create a synchronized array
+	
 	$scope.books = $firebaseArray(ref);
+	
 	$scope.orderProp = 'author';
+	
+
   }]);
+  
+bookusControllers.controller('AuthCtrl', ['$scope', '$firebaseAuth' ,
+  function($scope, $firebaseAuth) {
+	var ref = new Firebase("https://bookus.firebaseio.com/");
+	var auth = $firebaseAuth(ref);
+	
+	$scope.loginGoogle = function() {
+		console.log("Login with Google");
+		ref.authWithOAuthPopup("google", function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+			} else {
+				console.log("Authenticated successfully with payload:", authData);
+			}
+		});
+	};
+}]);
 
 bookusControllers.controller('BookDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
