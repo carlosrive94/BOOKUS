407e5e68bc9561498c42bc86d802232b23399092
David Guerrero <david.guerrero@est.fib.upc.edu>
2016-05-10 23:58:51 +0200

    delete category controller inside controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -151,17 +151,3 @@ bookusControllers.controller('UserCtrl', ['$scope', '$routeParams', '$firebaseOb
 			})
 		});
 }]);
-
-bookusControllers.controller('CategoryCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
-  	$scope.category = $routeParams.categoryId;
-  	$scope.books = [];
-  	var url;
-  	if ($routeParams.categoryId == "newest") url = 'https://www.googleapis.com/books/v1/volumes?q=orderBy=newest&key='+APIKey;
-  	else url = 'https://www.googleapis.com/books/v1/volumes?q=subject:'+$routeParams.categoryId+'&key='+APIKey; 
-	$http.get(url).success(function(data){
-			angular.forEach(data.items , function(rawBook){
-				$scope.books.push(getBook(rawBook));
-			});
-		});
-}]);
