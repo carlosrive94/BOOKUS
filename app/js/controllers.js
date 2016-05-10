5d957ce3748b0098100bfc6d55dec2c5add1db21
Carlos Rivero Molina <Carlos Rivero Molina>
2016-05-10 17:12:31 +0200

    Getting books form google books rest

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,14 +4,25 @@
 
 var bookusControllers = angular.module('bookusControllers', ["firebase"]);
 
-bookusControllers.controller('BookListCtrl', ['$scope', '$firebaseArray',
-  function($scope, $firebaseArray) {
-	var ref = new Firebase("https://bookus.firebaseio.com/");
-	
-	$scope.books = $firebaseArray(ref.child('books'));
-	
-	//$scope.orderProp = 'author';
+bookusControllers.controller('BookListCtrl', ['$scope', '$http',
+  function($scope, $http) {  
+	$http.get('https://www.googleapis.com/books/v1/volumes?q=comedy&maxResults=5'). //TODO
+		success(function(data){
+			$scope.books = [];
+			angular.forEach(data.items , function(rawBook){
+				$scope.books.push(getBook(rawBook));
+			});
+		});
 }]);
+
+function getBook(rawBook){
+	return {
+		id: rawBook.id,
+		title: rawBook.volumeInfo.title,
+		author: rawBook.volumeInfo.authors[0], //TODO
+		image: rawBook.volumeInfo.imageLinks.thumbnail
+	}
+}
   
 bookusControllers.controller('AuthCtrl', ['$scope', '$firebaseAuth' , '$firebaseObject' , '$firebaseArray',
   function($scope, $firebaseAuth, $firebaseObject, $firebaseArray) {
@@ -89,10 +100,12 @@ bookusControllers.controller('AuthCtrl', ['$scope', '$firebaseAuth' , '$firebase
 }]);
 
 
-bookusControllers.controller('BookDetailCtrl', ['$scope', '$routeParams', '$firebaseObject',
-  function($scope, $routeParams, $firebaseObject) {
-	var ref = new Firebase("https://bookus.firebaseio.com/books/"+$routeParams.bookId);
-	$scope.book = $firebaseObject(ref);
+bookusControllers.controller('BookDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+	$http.get('https://www.googleapis.com/books/v1/volumes?q='+$routeParams.bookId).
+		success(function(data){
+			$scope.book = getBook(data.items[0]); //There will be always just one object
+		});
 }]);
 
 bookusControllers.controller('UserCtrl', ['$scope', '$routeParams', '$firebaseObject', '$firebaseArray',
