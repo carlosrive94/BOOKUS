63b03fab7127dc23dc6244f6ceab24fc95712e6c
David Guerrero <david.guerrero@est.fib.upc.edu>
2016-05-12 20:10:40 +0200

    heart working in categories

---
+++ app/js/controllers.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/* Controllers */
+var APIKey = 'AIzaSyCtNBUQRvEOR-jmYBzg2oZ-H8IuI_pIc4Y';
+
+var bookusControllers = angular.module('bookusControllers', ["firebase"]);
+
+bookusControllers.controller('BookListCtrl', ['$scope', '$http',
+  function($scope, $http) {  
+	changeNav("navHome");
+	
+	$scope.newBooks = getBooks(null,true);
+	$scope.comedyBooks = getBooks("Comedy",false);
+	$scope.dramaBooks = getBooks("Drama",false);
+	$scope.fictionBooks = getBooks("Fiction",false)
+	
+	function getBooks(category,newest){
+		var books = [];
+		var url;
+		if(newest) url = 'https://www.googleapis.com/books/v1/volumes?q=orderBy=newest&maxResults=4&key='+APIKey;
+		else url = 'https://www.googleapis.com/books/v1/volumes?q=subject:'+category+'&orderBy=relevance&maxResults=4&key='+APIKey;
+		$http.get(url).success(function(data){
+				angular.forEach(data.items , function(rawBook){
+					books.push(getBook(rawBook));
+				});
+			});
+		return books;
+	};
+
+	$scope.likeBook = function (bookId){
+        if (document.getElementById(bookId).className == 'fa fa-heart-o'){
+        	document.getElementById(bookId).className = "fa fa-heart";
+        }
+        else{
+        	document.getElementById(bookId).className = "fa fa-heart-o";
+        }
+	}
+}]);
+
+function changeNav(currentNav){
+	document.getElementById("navHome").className = "";
+	document.getElementById("navMyBooks").className = "";
+	document.getElementById("navCategories").className = "";
+	document.getElementById("navNews").className = "";
+	document.getElementById(currentNav).className = "active";
+}
+
+function getBook(rawBook){
+	return {
+		id: rawBook.id,
+		title: rawBook.volumeInfo.title,
+		author: rawBook.volumeInfo.authors[0], //TODO
+		image: rawBook.volumeInfo.imageLinks.thumbnail,
+		publishedDate: rawBook.volumeInfo.publishedDate,
+		description: rawBook.volumeInfo.description,
+		averageRating: rawBook.volumeInfo.averageRating,
+		ratingsCount: rawBook.volumeInfo.ratingsCount,
+		pageCount: rawBook.volumeInfo.pageCount
+	}
+}
+
+bookusControllers.controller('BookDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {  
+	$http.get('https://www.googleapis.com/books/v1/volumes/'+$routeParams.bookId).
+		success(function(data){
+			$scope.book = getBook(data);
+		});
+}]);
