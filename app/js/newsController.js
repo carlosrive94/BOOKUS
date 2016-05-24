a3dc5058453a8da5230f89c5bb1c6b24374a80fd
David Guerrero <david.guerrero@est.fib.upc.edu>
2016-05-24 15:57:29 +0200

    first try rss news

--- app/js/newsController.js
+++ app/js/newsController.js
@@ -5,4 +5,33 @@
 bookusControllers.controller('NewsCtrl', ['$scope',
   function($scope) {
    changeNav("navNews");
-  }]);
+
+	var feedcontainer=document.getElementById("feeddiv");
+	var feedurl="http://www.fib.upc.edu/fib/rss.rss";
+	var feedlimit=8;
+	var rssoutput="<b>Latest News:</b><br /><ul>";
+
+	function rssfeedsetup(){
+		var feedpointer=new google.feeds.Feed(feedurl); //Google Feed API method
+		feedpointer.setNumEntries(feedlimit); //Google Feed API method
+		feedpointer.load(displayfeed); //Google Feed API method
+	}
+
+	function displayfeed(result){
+		if (!result.error){
+			var thefeeds=result.feed.entries;
+			for (var i=0; i<thefeeds.length; i++){
+				rssoutput+="<li><a href='" + thefeeds[i].link + "'>" + thefeeds[i].title + "</a></li>";
+				rssoutput+="</ul>";
+				feedcontainer.innerHTML=rssoutput;
+			}
+		}
+		else{
+			alert("Error fetching feeds!");
+		}
+	}
+
+	window.onload=function(){
+		rssfeedsetup();
+	}
+}]);
