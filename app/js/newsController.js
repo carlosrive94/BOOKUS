30993a0967bbeb22c31167c572fd2835b70de417
Carlos Rivero Molina <Carlos Rivero Molina>
2016-05-24 17:15:41 +0200

    Resolve error in news

--- app/js/newsController.js
+++ app/js/newsController.js
@@ -4,18 +4,16 @@
 
 bookusControllers.controller('NewsCtrl', ['$scope',
   function($scope) {
-   changeNav("navNews");
+	changeNav("navNews");
 
 	var feedcontainer=document.getElementById("feeddiv");
 	var feedurl="https://www.bookbrowse.com/rss/book_news.rss";
 	var feedlimit=8;
 	var rssoutput="<b>Latest News:</b><br /><ul>";
 
-	function rssfeedsetup(){
-		var feedpointer=new google.feeds.Feed(feedurl); //Google Feed API method
-		feedpointer.setNumEntries(feedlimit); //Google Feed API method
-		feedpointer.load(displayfeed); //Google Feed API method
-	}
+	var feedpointer=new google.feeds.Feed(feedurl); //Google Feed API method
+	feedpointer.setNumEntries(feedlimit); //Google Feed API method
+	feedpointer.load(displayfeed); //Google Feed API method
 
 	function displayfeed(result){
 		if (!result.error){
@@ -30,9 +28,5 @@ bookusControllers.controller('NewsCtrl', ['$scope',
 		else{
 			alert("Error fetching feeds!");
 		}
-	}
-
-	window.onload=function(){
-		rssfeedsetup();
 	}
 }]);
