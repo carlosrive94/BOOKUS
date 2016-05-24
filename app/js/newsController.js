fa2a144e7682bcb04d9477646b36a802bea42b99
David Guerrero <david.guerrero@est.fib.upc.edu>
2016-05-24 17:02:58 +0200

    style news

--- app/js/newsController.js
+++ app/js/newsController.js
@@ -9,7 +9,7 @@ bookusControllers.controller('NewsCtrl', ['$scope',
 	var feedcontainer=document.getElementById("feeddiv");
 	var feedurl="https://www.bookbrowse.com/rss/book_news.rss";
 	var feedlimit=8;
-	var rssoutput="<b>Latest News:</b><br /><ul>";
+	var rssoutput="<h2>Latest News:</h2><br /><ul>";
 
 	function rssfeedsetup(){
 		var feedpointer=new google.feeds.Feed(feedurl); //Google Feed API method
@@ -21,11 +21,12 @@ bookusControllers.controller('NewsCtrl', ['$scope',
 		if (!result.error){
 			var thefeeds=result.feed.entries;
 			for (var i=0; i<thefeeds.length; i++){
-				rssoutput+="<li><a href='" + thefeeds[i].link + "'>" + thefeeds[i].title + "</a></li>";
+				rssoutput+="<li><h3><a href='" + thefeeds[i].link + "'>" + thefeeds[i].title + "</a></h3></li>";
 				rssoutput+="<p>" + thefeeds[i].content + "</p>";
-				rssoutput+="</ul>";
-				feedcontainer.innerHTML=rssoutput;
+				
 			}
+			rssoutput+="</ul>";
+			feedcontainer.innerHTML=rssoutput;
 		}
 		else{
 			alert("Error fetching feeds!");
