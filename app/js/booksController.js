973c4e5a10d73b02e151dbaa7c89071982188321
David Guerrero <david.guerrero@est.fib.upc.edu>
2016-05-27 16:14:57 +0200

    twitter share

--- app/js/booksController.js
+++ app/js/booksController.js
@@ -48,6 +48,16 @@ bookusControllers.controller('BookDetailCtrl', ['$scope', '$routeParams', '$http
 	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=188198967918131";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
+
+	//Twitter Share Configuration
+	// Load twitter sharing button
+    (function(d,s,id){
+        var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+        if(!d.getElementById(id)){
+            js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }(document, 'script', 'twitter-wjs'));
 }]);
 
 bookusControllers.controller('BookSearchCtrl', ['$scope', '$http', '$location',
